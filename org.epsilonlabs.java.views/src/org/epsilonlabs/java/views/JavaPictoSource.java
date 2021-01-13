31c8d4163e4bd6743c266b57202b833f78eed995
Betty <basp91@gmail.com>
2021-01-13 13:05:44 +0000

    improved method signature and added abstract class support

--- org.epsilonlabs.java.views/src/org/epsilonlabs/java/views/JavaPictoSource.java
+++ org.epsilonlabs.java.views/src/org/epsilonlabs/java/views/JavaPictoSource.java
@@ -11,13 +11,14 @@ import org.eclipse.epsilon.picto.ViewTree;
 import org.eclipse.epsilon.picto.source.PictoSource;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.ILocalVariable;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.internal.core.ImportContainer;
 import org.eclipse.jdt.internal.core.SourceType;
+import org.eclipse.jdt.internal.corext.util.JdtFlags;
 import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor;
+import org.eclipse.jdt.ui.JavaElementLabels;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.part.FileEditorInput;
 
@@ -42,56 +43,65 @@ public class JavaPictoSource implements PictoSource{
 	}
 
 	public String execute() {
-		//final IJavaProject model = element.getJavaProject();
-		String result = "classDiagram";
-		result += System.lineSeparator();
+		final StringBuffer buffer = new StringBuffer();
+		buffer.append("classDiagram");
+		buffer.append(System.lineSeparator());
 		try {
 			if (element instanceof ICompilationUnit) {
 				ICompilationUnit cu = (ICompilationUnit) element;
 				final IJavaElement[] children = cu.getChildren();
 				for (IJavaElement ch : children) {
-					result = processInterface(result, ch);
+					buffer.append(processElement(ch));
 				}
 			}
 		} catch (JavaModelException e) {
 			e.printStackTrace();
 		}
-		return result ;
+		return buffer.toString();
 	}
 
-	protected String processInterface(String buffer, IJavaElement jElement) throws JavaModelException {
+	protected String processElement(IJavaElement jElement) throws JavaModelException {
+		final StringBuilder buffer = new StringBuilder();
 		Map<String, SourceType> map = new HashMap<>(); 
 		if (jElement instanceof SourceType) {
 			SourceType st = (SourceType) jElement;
-				buffer += processInterfaceInternal(st);
-				buffer += System.lineSeparator();								
+				buffer.append(buildClass(st));
+				buffer.append(System.lineSeparator());								
 				final String superclassName = st.getSuperclassName();
 				if (superclassName != null) {					
 					final IType findType = jElement.getJavaProject().findType(superclassName);
-					if (findType instanceof SourceType) {					
-						buffer += processInterfaceInternal((SourceType) findType);
-						buffer += System.lineSeparator();								
+					if (findType instanceof SourceType) {		
+						buffer.append(buildClass((SourceType) findType));
+						buffer.append(System.lineSeparator());								
 					}
 				}
 				//Extension
 				List<String> ok = new ArrayList<>();
 				if (st.isClass()) {					
 					if (st.getSuperclassName() !=null) {
-						buffer += st.getSuperclassName() + " <|-- " + st.getElementName();						
-						buffer += System.lineSeparator();
-						buffer += processFromPackage(st, map, st.getSuperclassName());
+						String inheritance = st.getSuperclassName() + " <|-- " + st.getElementName();
+						buffer.append(inheritance);
+						buffer.append(System.lineSeparator());								
+						StringBuilder processFromPackage = processFromPackage(st, map, st.getSuperclassName());
+						if (processFromPackage != null) {
+							buffer.append(processFromPackage);
+							buffer.append(System.lineSeparator());								
+						}
 						ok.add(st.getSuperclassName());
-						buffer += System.lineSeparator();
 					}
 				}
 
 				// Implementation
 				for (String interf : st.getSuperInterfaceNames()) {
-					buffer += interf + " <|.. " + st.getElementName();
-					buffer += System.lineSeparator();
+					String realization = interf + " <|.. " + st.getElementName();
+					buffer.append(realization);
+					buffer.append(System.lineSeparator());								
 					if (!map.containsKey(st.getSuperclassName())) {
-						buffer += processFromPackage(st, map, interf);
-						buffer += System.lineSeparator();
+						StringBuilder process = processFromPackage(st, map, interf);
+						if (process != null) {
+							buffer.append(process);					
+							buffer.append(System.lineSeparator());								
+						}
 					}
 					ok.add(interf);
 				}
@@ -112,94 +122,65 @@ public class JavaPictoSource implements PictoSource{
 						final String[] split = c.getElementName().split("\\.");
 						final SourceType imSt = (SourceType) e;
 						map.put(split[split.length-1], imSt);
-						buffer += processInterfaceInternal(imSt);
-						buffer += System.lineSeparator();								
+						StringBuilder process = buildClass(imSt);
+						if (process != null) {
+							buffer.append(process);
+							buffer.append(System.lineSeparator());								
+						}
 					}
 				}
 			}
 		}
-		return buffer;
+		return buffer.toString();
 	}
 
-	/**
-	 * @param buffer
-	 * @param jElement
-	 * @param map
-	 * @param st
-	 * @param ok
-	 * @return
-	 * @throws JavaModelException
-	 */
-	protected String processFromPackage(SourceType jElement, Map<String, SourceType> map,
+	protected StringBuilder processFromPackage(SourceType jElement, Map<String, SourceType> map,
 			String elemname) throws JavaModelException {
-		String buffer = "";
 		if (!map.containsKey(elemname)) {
 			String name = jElement.getPackageFragment().getElementName() +"."+ elemname;
 			final IType findType = jElement.getJavaProject().findType(name);
 			if (findType instanceof SourceType) {		
-				buffer += processInterfaceInternal((SourceType) findType);
+				return buildClass((SourceType) findType);
 			}
 		}
-		return buffer;
+		return null;
 	}
 
-	/**
-	 * @param buffer
-	 * @param cu
-	 * @param st
-	 * @return
-	 * @throws JavaModelException
-	 */
-	protected String processInterfaceInternal(SourceType st)
+	protected StringBuilder buildClass(SourceType st)
 			throws JavaModelException {
-		String buffer = "";
-		buffer += "class ";
-		buffer += st.getElementName();
-		buffer += " {";
-		buffer += System.lineSeparator();
+		final StringBuilder buffer = new StringBuilder();
+		buffer.append(String.format("class %s {", st.getElementName()));
+		buffer.append(System.lineSeparator());
 		if (st.isInterface()) {
-			buffer += "<<interface>>";
-			buffer += System.lineSeparator();				
+			buffer.append("<<interface>>");
+			buffer.append(System.lineSeparator());				
 		} else if (st.isEnum()) {
-			buffer += "<<enumeration>>";
-			buffer += System.lineSeparator();
-		} 
+			buffer.append("<<enumeration>>");
+			buffer.append(System.lineSeparator());
+		} else if (JdtFlags.isAbstract(st)) {
+			buffer.append("<<abstract>>");
+			buffer.append(System.lineSeparator());
+		}
+		
+		// Methods
 		final IMethod[] methods = st.getMethods();
 		for (IMethod m : methods) {
-			String methodSignature = "";
-			methodSignature += "+" + m.getElementName() + "(";
-			
-			// Parameters
-			final ILocalVariable[] parameters = m.getParameters();
-			for (int i=0; i< parameters.length; i++) {
-				final ILocalVariable p = parameters[i];
-				methodSignature += p.getElementName() + " : " + type(p.getTypeSignature());
-				if (i != parameters.length-1) {
-					methodSignature += ", ";
-				}
-			}
-			methodSignature += ")";
-			if (m.getReturnType().equals("V")) {
-				methodSignature+= " void";
-			}  else {
-				methodSignature += " " + type(m.getReturnType());
-			}
-			buffer += methodSignature.replace(">", "~").replace("<", "~");
-			buffer += System.lineSeparator();
+			StringBuffer methodSignature = new StringBuffer();
+			JavaElementLabels.getMethodLabel(m, JavaElementLabels.ALL_DEFAULT | JavaElementLabels.M_PARAMETER_NAMES | JavaElementLabels.M_APP_RETURNTYPE, methodSignature);
+			buffer.append(methodSignature.toString().replaceAll("[<>]", "~").replace(":", ""));
+			buffer.append(System.lineSeparator());
 		}
-		buffer += "}";
-		return buffer;
+		return buffer.append("}");
 	}
 	
 	protected String type(String a) {
-		return a.replace("Q", "").replace(";", "");
+		return a.replaceAll("[<>]", "~");
 	}
 	
 	@Override
 	public ViewTree getViewTree(IEditorPart editorPart) throws Exception {
 		ViewTree viewTree = new ViewTree();
 		viewTree.setPromise(new StaticContentPromise(execute()));
-		//viewTree.setFormat("graphviz-dot");
 		viewTree.setFormat("text");
 		return viewTree;
 	}
