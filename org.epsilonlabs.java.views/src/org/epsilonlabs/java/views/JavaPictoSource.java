4cfa3bf85671a6ed1115a98d5c65ce185ea465cf
Betty Sanchez <>
2021-04-13 16:57:20 +0100

    rendering as mermaid

--- org.epsilonlabs.java.views/src/org/epsilonlabs/java/views/JavaPictoSource.java
+++ org.epsilonlabs.java.views/src/org/epsilonlabs/java/views/JavaPictoSource.java
@@ -1,18 +1,23 @@
 package org.epsilonlabs.java.views;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.eclipse.epsilon.picto.StaticContentPromise;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.epsilon.picto.ViewContent;
 import org.eclipse.epsilon.picto.ViewTree;
 import org.eclipse.epsilon.picto.source.PictoSource;
+import org.eclipse.epsilon.picto.transformers.ExternalContentTransformation;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.ITypeHierarchy;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.internal.core.ImportContainer;
 import org.eclipse.jdt.internal.core.SourceType;
@@ -112,6 +117,22 @@ public class JavaPictoSource implements PictoSource {
 				 * }
 				 */
 			}
+			// Subtypes
+			IType type= (IType)st;
+			final ITypeHierarchy newTypeHierarchy = type.newTypeHierarchy(new NullProgressMonitor());
+			final IType[] subclasses = newTypeHierarchy.getSubtypes(st);
+			for (IType s : subclasses) {
+				String inheritance = st.getElementName() + " <|-- " + s.getElementName();
+				buffer.append(inheritance);
+				buffer.append(System.lineSeparator());
+				if (s instanceof SourceType) {						
+					StringBuilder process = buildClass((SourceType)s);
+					if (process != null) {
+						buffer.append(process);
+						buffer.append(System.lineSeparator());
+					}
+				}
+			}
 
 		} else if (jElement instanceof ImportContainer) {
 			ImportContainer ic = (ImportContainer) jElement;
@@ -124,16 +145,20 @@ public class JavaPictoSource implements PictoSource {
 						final String[] split = c.getElementName().split("\\.");
 						final SourceType imSt = (SourceType) e;
 						map.put(split[split.length - 1], imSt);
-						StringBuilder process = buildClass(imSt);
+						/*StringBuilder process = buildClass(imSt, false);
 						if (process != null) {
 							buffer.append(process);
 							buffer.append(System.lineSeparator());
-						}
+						}*/
 					}
 				}
 			}
 		}
 		return buffer;
+	}
+	
+	protected StringBuilder buildClass(SourceType st) throws JavaModelException {
+		return buildClass(st, true);
 	}
 
 	protected StringBuilder processFromPackage(SourceType jElement, Map<String, SourceType> map, String elemname)
@@ -148,12 +173,12 @@ public class JavaPictoSource implements PictoSource {
 		return null;
 	}
 
-	protected StringBuilder buildClass(SourceType st) throws JavaModelException {
+	protected StringBuilder buildClass(SourceType st, boolean includeMethods) throws JavaModelException {
 		final StringBuilder buffer = new StringBuilder();
 		buffer.append(String.format("class %s {", st.getElementName()));
 		buffer.append(System.lineSeparator());
 		if (st.isInterface()) {
-			buffer.append("<<interface>>");
+			buffer.append("<<Interface>>");
 			buffer.append(System.lineSeparator());
 		} else if (st.isEnum()) {
 			buffer.append("<<enumeration>>");
@@ -164,25 +189,86 @@ public class JavaPictoSource implements PictoSource {
 		}
 
 		// Methods
-		final IMethod[] methods = st.getMethods();
-		for (IMethod m : methods) {
-			StringBuffer methodSignature = new StringBuffer();
-			JavaElementLabels.getMethodLabel(m, JavaElementLabels.ALL_DEFAULT | JavaElementLabels.M_PARAMETER_NAMES
-					| JavaElementLabels.M_APP_RETURNTYPE, methodSignature);
-			buffer.append(methodSignature.toString().replaceAll("[<>]", "~").replace(":", ""));
-			buffer.append(System.lineSeparator());
+		if (includeMethods) {
+			final IMethod[] methods = st.getMethods();
+			for (IMethod m : methods) {
+				if (!JdtFlags.isPrivate(m)){				
+					StringBuffer methodSignature = new StringBuffer();
+					if (st.isClass()) {	
+						if (JdtFlags.isPublic(m)) {
+							methodSignature.append("+");
+						} else  if (JdtFlags.isProtected(m)) {
+							//methodSignature.append("#");
+						}
+					}
+					JavaElementLabels.getMethodLabel(m, JavaElementLabels.ALL_DEFAULT | JavaElementLabels.M_PARAMETER_NAMES
+							| JavaElementLabels.M_APP_RETURNTYPE, methodSignature);
+					
+					String replacement = "";
+					if (st.isClass()) {					
+						replacement = JdtFlags.isAbstract(m) ? "*" : (JdtFlags.isStatic(m) ? "$" : "");
+					} 
+					String signature = methodSignature.toString();
+					final String[] split = signature.split("<");
+					if (split.length>1) {
+						signature = String.format("%s<%s>", split[0], split[1]/*.replace("(.*)( extends )(.*)", "$3")*/);
+					}
+					signature = signature.toString()
+							.replaceAll("[<>]", "~") // In Mermaid generics use this character
+							.replace(":", replacement) // Mermaid adds the colon
+							.replace("~?", replacement) // Mermaid adds the colon
+							.replaceAll("([?]( extends )?)", "") // question marks break the script
+							;
+					buffer.append(signature);
+					buffer.append(System.lineSeparator());
+				}
+			}
 		}
 		return buffer.append("}");
 	}
 
 	@Override
 	public ViewTree getViewTree(IEditorPart editorPart) throws Exception {
+		//final StaticContentPromise content = new StaticContentPromise(text);
+		//viewTree.setPromise();
+		final String text = execute();
+		
 		ViewTree viewTree = new ViewTree();
-		viewTree.setPromise(new StaticContentPromise(execute()));
-		viewTree.setFormat("text");
+		viewTree.setContent(mermaid(text));
+		viewTree.setFormat("html");
 		return viewTree;
 	}
 
+	private ViewContent mermaid(String text){
+		final ViewContent content = new ViewContent("text", text);
+		String html = null;
+		String mmdString = content.getText();
+		try {
+			final Path mmd = ExternalContentTransformation.createTempFile("mm", mmdString.getBytes());
+			final Path imgTmp = mmd.getParent().resolve(mmd.getFileName()+".svg");
+			String program = "/usr/local/bin/mmdc";
+			html = new String(new ExternalContentTransformation(
+				imgTmp, program, "-i", mmd, "-o", imgTmp
+			).call());
+			return new ViewContent("svg", html, content);
+		}
+		catch (IOException iox) {
+		}
+		html = "<!DOCTYPE html>\n" + 
+				"<html>\n" + 
+				"<head>\n" +
+				"<meta charset=\"UTF-8\"/>"+
+				"  <title>A Meaningful Page Title</title></head>"
+				+ "<body>"
+				+ "<div class=\"mermaid\">\n" + mmdString.replaceAll("<", "&lt;").replaceAll(">", "&gt;") + "\n</div>"
+				+ "<script src=\"https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js\"></script>\n" 
+				+ "<script>mermaid.initialize({startOnLoad:true});</script>"
+						+ "</body>"
+						+ "</html>";
+		return new ViewContent("html", html, content);	
+
+	}
+	
 	@Override
 	public void dispose() {
 		// do nothing
